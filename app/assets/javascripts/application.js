54cd14d3bb38bc3237554e55b3def7c9612f26c0
Tony <ANCLIST@GMAIL.COM>
2019-05-19 15:23:12 -0400

    Dynamic height sticky footer

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,21 @@
 //= require popper
 //= require bootstrap
 //= require_tree .
+
+
+var bumpIt = function() {
+      $('body').css('margin-bottom', $('.footer').height());
+    },
+    didResize = false;
+
+bumpIt();
+
+$(window).resize(function() {
+  didResize = true;
+});
+setInterval(function() {
+  if(didResize) {
+    didResize = false;
+    bumpIt();
+  }
+}, 250);
