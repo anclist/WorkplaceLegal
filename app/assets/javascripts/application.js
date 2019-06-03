b268445632396be4a922440fa0cef65d45a873c8
Tony <ANCLIST@GMAIL.COM>
2019-06-03 16:05:09 -0400

    alerts fade and close after 2 seconds

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -34,3 +34,9 @@ setInterval(function() {
     bumpIt();
   }
 }, 250);
+
+window.setTimeout(function() {
+    $(".alert").fadeTo(500, 0).slideUp(500, function(){
+        $(this).remove();
+    });
+}, 4000);
